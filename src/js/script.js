2cd824d80207d1ad72e473b30631e165bdba408c
roslove44 <rostandmigan68@gmail.com>
2024-01-18 14:47:34 +0100

    Hamburger Menu

--- src/js/script.js
+++ src/js/script.js
@@ -1,20 +1,13 @@
-let menuOpenBtn = document.querySelector("#menuOpenBtn");
-let menuCloseBtn = document.querySelector("#menuCloseBtn");
-let mobileMenu = document.querySelector("#mobileMenu");
+const initApp = () => {
+  let mobileMenu = document.querySelector("#mobileMenu");
+  let hamburgerBtn = document.querySelector("#hamburger-btn");
 
-menuOpenBtn.addEventListener("click", () => {
-  mobileMenu.classList.remove("hidden");
-  menuCloseBtn.classList.remove("hidden");
-  menuOpenBtn.classList.add("hidden");
-  mobileMenu.classList.add("menu-open-animation");
-});
+  const toggleMenu = () => {
+    mobileMenu.classList.toggle("hidden");
+    hamburgerBtn.classList.toggle("toggle-btn");
+  };
 
-menuCloseBtn.addEventListener("click", () => {
-  mobileMenu.classList.add("menu-close-animation");
-  setTimeout(() => {
-    mobileMenu.classList.add("hidden");
-    menuCloseBtn.classList.add("hidden");
-    menuOpenBtn.classList.remove("hidden");
-    mobileMenu.classList.remove("menu-close-animation");
-  }, 250);
-});
+  hamburgerBtn.addEventListener("click", toggleMenu);
+};
+
+document.addEventListener("DOMContentLoaded", initApp);
