8cf0f1d15ac852da97fec7f9ad0a942484e995b4
roslove44 <rostandmigan68@gmail.com>
2024-04-11 02:57:18 +0200

    Remake

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,25 +13,6 @@ module.exports = {
           "80%": { transform: "scaleY(1.2)" },
           "100%": { transform: "scaleY(1)" },
         },
-        "say-hi": {
-          "0%": { transform: "translateZ(0) scale(1)" },
-          "15%": {
-            transform: "translate3d(-25%, 0,0) rotate(-5deg) scale(1.1)",
-          },
-          "30%": {
-            transform: "translate3d(20%, 0,0) rotate(3deg) scale(1.2)",
-          },
-          "45%": {
-            transform: "translate3d(-15%, 0,0) rotate(-3deg) scale(1.3)",
-          },
-          "60%": {
-            transform: "translate3d(10%, 0,0) rotate(2deg) scale(1.2)",
-          },
-          "75%": {
-            transform: "translate3d(-5%, 0,0) rotate(-1deg) scale(1.1)",
-          },
-          "100%": { transform: "translateZ(0) scale(1)" },
-        },
         "portfolio-action": {
           "0%": { transform: "scaleX(0)", opacity: 0.0 },
           "20%": { transform: "scaleX(0.2)", opacity: 0.8 },
@@ -43,7 +24,6 @@ module.exports = {
       },
       animation: {
         "open-menu": "open-menu 0.5s ease-in-out forwards",
-        "say-hi": "say-hi 2.5s infinite",
         "portfolio-action": "portfolio-action 0.3s ease-in-out forwards",
       },
     },
