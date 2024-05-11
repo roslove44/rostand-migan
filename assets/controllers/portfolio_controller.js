d8478f63c1acab50c1c95b4463da2094ca5e51c6
roslove44 <rostandmigan68@gmail.com>
2024-05-12 01:16:47 +0200

    Fix Portfolio Filter

--- assets/controllers/portfolio_controller.js
+++ assets/controllers/portfolio_controller.js
@@ -1,36 +1,22 @@
 import { Controller } from "@hotwired/stimulus";
 
-/*
- * This is an example Stimulus controller!
- *
- * Any element with a data-controller="hello" attribute will cause
- * this controller to be executed. The name "hello" comes from the filename:
- * hello_controller.js -> "hello"
- *
- * Delete this file or adapt it for your use!
- */
 export default class extends Controller {
   static targets = ["portfolioProjects", "filterActions"];
-  connect() {
-    console.log(
-      "Hello Stimulus! Edit me in assets/controllers/hello_controller.js"
-    );
-  }
+  connect() {}
 
   filter(event) {
     const projects = this.portfolioProjectsTarget.children;
     const button = event.currentTarget;
-    this.setButtonActive(button);
+    this.#setButtonActive(button);
     const filter = event.currentTarget.dataset.filter;
     if (filter == "*") {
-      this.showAll(projects);
+      this.#showAll(projects);
     } else {
-      console.log(filter);
-      this.showSpecificCategory(projects, filter);
+      this.#showSpecificCategory(projects, filter);
     }
   }
 
-  setButtonActive(button) {
+  #setButtonActive(button) {
     if (button && button.classList.contains("active")) {
       return;
     }
@@ -43,35 +29,32 @@ export default class extends Controller {
     button.classList.add("active");
   }
 
-  showAll(projects) {
+  #showAll(projects) {
     for (const project of projects) {
       project.classList.remove("isNotVisible");
-      project.classList.add("animate-portfolio-action");
+      project.style.display = "none";
+      project.classList.remove("animate-portfolio-action");
+
       setTimeout(() => {
         project.style.display = "flex";
+        project.classList.add("animate-portfolio-action");
       }, 300);
     }
   }
 
-  showSpecificCategory(projects, filter) {
+  #showSpecificCategory(projects, filter) {
     for (const project of projects) {
+      project.classList.add("isNotVisible");
+      project.classList.remove("animate-portfolio-action");
+      project.style.display = "none";
+
       if (project.classList.contains(filter)) {
         project.classList.remove("isNotVisible");
-        // project.classList.add("animate-portfolio-action");
         setTimeout(() => {
           project.style.display = "flex";
+          project.classList.add("animate-portfolio-action");
         }, 300);
-      } else {
-        project.classList.add("isNotVisible");
       }
     }
-
-    setTimeout(() => {
-      for (const project of projects) {
-        if (project.classList.contains("isNotVisible")) {
-          project.style.display = "none";
-        }
-      }
-    }, 300);
   }
 }
