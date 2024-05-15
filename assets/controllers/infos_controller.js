b58a00e2106ba33d0e0ead036bef0608e5611779
roslove44 <rostandmigan68@gmail.com>
2024-05-15 17:47:35 +0200

    Info API : POST GET DELETE

--- assets/controllers/infos_controller.js
+++ assets/controllers/infos_controller.js
@@ -1,15 +1,16 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["infosContainer"];
+  static targets = ["infosContainer", "infosCount"];
   connect() {
     this.addFormModel = document.getElementById("addFormModel").content;
     this.messageModel = document.getElementById("messageModel").content;
     this.infoViewModel = document.getElementById("infoViewModel").content;
+    this._narnia = document.querySelector('input[name="_narnia"]').value;
 
     addEventListener("submit", (event) => {
       event.preventDefault();
-      const _token = event.submitter.dataset.token;
+      const _token = this._narnia;
       const clef = event.target.elements.clef.value;
       const value = event.target.elements.value.value;
 
@@ -59,7 +60,7 @@ export default class extends Controller {
     }
   }
 
-  addInfoView(clef, value, id) {
+  addInfoView(clef, value) {
     const infoViewModel = this.infoViewModel.cloneNode(true);
     infoViewModel.querySelector("tr").removeAttribute("data-gu");
     infoViewModel.querySelector("tr").removeAttribute("data-token");
@@ -67,10 +68,46 @@ export default class extends Controller {
     infoViewModel.querySelector("tr .value").textContent = value;
     this.infosContainerTarget.prepend(infoViewModel);
   }
-  deleteInfoView() {}
+  #infoToDelete(event) {
+    return event.target.parentNode.parentNode;
+  }
+
+  deleteInfo(event) {
+    if (confirm("Voulez vous vraiment supprimé cet info ?")) {
+      const target =
+        this.#infoToDelete(event).querySelector(".clef").textContent;
+      const delete_url = "/api/info/delete/" + target;
+      const data = { _token: this._narnia };
+      // Configuration de la requête
+      const requestOptions = {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      };
+      fetch(delete_url, requestOptions)
+        .then((response) => {
+          // if (!response.ok) {throw new Error("Erreur lors de la requête");}
+          return response.json();
+        })
+        .then((data) => {
+          if (data.error) {
+            this.addFlash("danger", data.message);
+            return;
+          }
+          this.#infoToDelete(event).remove();
+          console.log("Réponse reçue :", data);
+          this.addFlash("success", data.message);
+        })
+        .catch((error) => {
+          this.addFlash("danger", "Une erreur technique s'est produite");
+        });
+    }
+  }
 
   #postInfo(data) {
-    const post_url = this.addFormModel.querySelector("tr").dataset.pu;
+    const post_url = "/api/info/post/";
     // Configuration de la requête
     const requestOptions = {
       method: "POST",
@@ -85,13 +122,14 @@ export default class extends Controller {
         return response.json();
       })
       .then((data) => {
-        data = JSON.parse(data);
         if (data.error) {
-          this.addFlash("danger", data.error);
+          this.addFlash("danger", data.message);
           return;
         }
         this.removeAddForm();
         console.log("Réponse reçue :", data);
+        const info = data.content;
+        this.addInfoView(info.clef, info.value);
         this.addFlash("success", data.message);
       })
       .catch((error) => {
@@ -100,20 +138,11 @@ export default class extends Controller {
   }
 
   #getInfos() {
-    const get_url = this.infoViewModel.querySelector("tr").dataset.gu;
-    const _token = this.infoViewModel.querySelector("tr").dataset.token;
-    const data = { _token: _token };
-    // Configuration de la requête
-    const requestOptions = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    };
+    const get_url = "/api/info/get/";
+
     this.addFlash("warning", "En cours........");
 
-    fetch(get_url, requestOptions)
+    fetch(get_url)
       .then((response) => {
         // if (!response.ok) {throw new Error("Network response was not ok " + response.statusText);}
         this.removeAllFlash();
@@ -121,16 +150,18 @@ export default class extends Controller {
       })
       .then((data) => {
         if (data.error) {
-          this.addFlash("danger", data.error);
+          this.addFlash("danger", data.message);
           return;
         }
-        data = JSON.parse(data);
-        if (!data.content.length) {
+        console.log(data);
+        const content = data.content;
+        if (!content.length) {
+          this.infosCountTarget.textContent = "0 info";
           this.addFlash("warning", "Aucune clé enregistrée pour le moment");
           return;
         }
-        console.log(data.content);
-        data.content.map((info) => this.addInfoView(info.clef, info.value));
+        this.infosCountTarget.textContent = content.length + " " + "info(s)";
+        content.map((info) => this.addInfoView(info.clef, info.value));
       })
       .catch((error) => {
         this.addFlash("danger", "Une erreur technique s'est produite" + error);
--- src/Controller/Admin/InfoController.php
+++ src/Controller/Admin/InfoController.php
@@ -15,86 +15,124 @@ use Symfony\Component\Serializer\SerializerInterface;
 
 class InfoController extends AbstractController
 {
-    #[Route('/dashboard/info/post/', name: 'admin_info_post', methods: ['POST'])]
+    #[Route('/api/info/post/', name: 'admin_info_post', methods: ['POST'])]
     public function addInfo(Request $request, SerializerInterface $serializer, EntityManagerInterface $em): JsonResponse
     {
         $data = json_decode($request->getContent(), true);
-        $info = new Info();
 
-        if ($this->isCsrfTokenValid('postInfo' . $this->getUser()->getUserIdentifier(), $data['_token'])) {
-            try {
-                $info->setClef(strtolower($this->purify_input($data['clef'])));
-                $info->setValue($this->purify_input($data['value']));
-                $em->persist($info);
-                $em->flush();
-
-                return $this->postReturn(
-                    serializer: $serializer,
-                    responseCode: Response::HTTP_OK,
-                    message: "Info enregistré avec succès."
-                );
-            } catch (UniqueConstraintViolationException $e) {
-                return $this->postReturn(
-                    serializer: $serializer,
-                    responseCode: Response::HTTP_CONFLICT,
-                    error: "La clé renseignée est déjà enregistrée."
-                );
-            } catch (\Throwable $th) {
-                return $this->postReturn(
-                    serializer: $serializer,
-                    responseCode: Response::HTTP_INTERNAL_SERVER_ERROR,
-                    error: $th->getMessage()
-                );
-            }
-        } else {
+        if (!$this->isCsrfTokenValid('info' . $this->getUser()->getUserIdentifier(), $data['_token'])) {
             return $this->postReturn(
                 serializer: $serializer,
                 responseCode: Response::HTTP_UNAUTHORIZED,
-                error: "Token Invalide"
+                message: "Token Invalide",
+                error: true
+            );
+        }
+        $info = new Info();
+        try {
+            $info->setClef(strtolower($this->purify_input($data['clef'])));
+            $info->setValue($this->purify_input($data['value']));
+            $em->persist($info);
+            $em->flush();
+
+            return $this->postReturn(
+                serializer: $serializer,
+                responseCode: Response::HTTP_OK,
+                message: "Info enregistré avec succès.",
+                content: $info
+            );
+        } catch (UniqueConstraintViolationException $e) {
+            return $this->postReturn(
+                serializer: $serializer,
+                responseCode: Response::HTTP_CONFLICT,
+                message: "La clé renseignée est déjà enregistrée.",
+                error: true
+            );
+        } catch (\Throwable $th) {
+            return $this->postReturn(
+                serializer: $serializer,
+                responseCode: Response::HTTP_INTERNAL_SERVER_ERROR,
+                message: $th->getMessage(),
+                error: true
             );
         }
     }
 
-    #[Route('/dashboard/info/get/', name: 'admin_info_get', methods: ['POST'])]
+    #[Route('/api/info/get/', name: 'admin_info_get', methods: ['GET'])]
     public function getInfo(Request $request, SerializerInterface $serializer, InfoRepository $infoRepository): JsonResponse
+    {
+
+        try {
+            $infos = $infoRepository->findAll();
+
+            return $this->postReturn(
+                serializer: $serializer,
+                responseCode: Response::HTTP_OK,
+                message: "Ensemble des infos",
+                content: $infos
+            );
+        } catch (\Throwable $th) {
+            return $this->postReturn(
+                serializer: $serializer,
+                responseCode: Response::HTTP_INTERNAL_SERVER_ERROR,
+                message: $th->getMessage(),
+                error: true
+            );
+        }
+    }
+
+    #[Route('/api/info/delete/{clef}', name: 'admin_info_delete', methods: ['DELETE'])]
+    public function deleteInfo($clef, InfoRepository $infoRepository, Request $request, SerializerInterface $serializer, EntityManagerInterface $em): JsonResponse
     {
         $data = json_decode($request->getContent(), true);
 
-        if ($this->isCsrfTokenValid('postInfo' . $this->getUser()->getUserIdentifier(), $data['_token'])) {
-            try {
-                $infos = $infoRepository->findAll();
-
-                return $this->postReturn(
-                    serializer: $serializer,
-                    responseCode: Response::HTTP_OK,
-                    message: "Ensemble des infos",
-                    content: $infos
-                );
-            } catch (\Throwable $th) {
-                return $this->postReturn(
-                    serializer: $serializer,
-                    responseCode: Response::HTTP_INTERNAL_SERVER_ERROR,
-                    error: $th->getMessage()
-                );
-            }
-        } else {
+        if (!$this->isCsrfTokenValid('info' . $this->getUser()->getUserIdentifier(), $data['_token'])) {
             return $this->postReturn(
                 serializer: $serializer,
                 responseCode: Response::HTTP_UNAUTHORIZED,
-                error: "Token Invalide"
+                message: "Token Invalide",
+                error: true
+            );
+        }
+
+        $info = $infoRepository->findOneBy(['clef' => $clef]);
+        if (!$info) {
+            return $this->postReturn(
+                serializer: $serializer,
+                responseCode: Response::HTTP_UNAUTHORIZED,
+                message: "Aucune clé ne correspond à votre demande",
+                error: true
+            );
+        }
+        try {
+            $em->remove($info);
+            $em->flush();
+
+            return $this->postReturn(
+                serializer: $serializer,
+                responseCode: Response::HTTP_OK,
+                message: "Info supprimé avec succès."
+            );
+        } catch (\Throwable $th) {
+            return $this->postReturn(
+                serializer: $serializer,
+                responseCode: Response::HTTP_INTERNAL_SERVER_ERROR,
+                message: $th->getMessage(),
+                error: true
             );
         }
     }
 
-    private function postReturn(SerializerInterface $serializer, int $responseCode, ?string $error = null,  ?string $message = null, null|array|Info $content = null): JsonResponse
+    private function postReturn(SerializerInterface $serializer, int $responseCode, bool $error = false,  ?string $message = null, null|array|Info $content = null): JsonResponse
     {
-        $result = [
-            "message" => $message,
-            "content" => $content,
-            "error" => $error,
-        ];
-        $result = $serializer->serialize($result, 'json');
-        return $this->json($result, $responseCode);
+        return $this->json(
+            [
+                "message" => $message,
+                "content" => $content,
+                "error" => $error,
+                "status" => $responseCode
+            ]
+        );
     }
 
     private function purify_input($input)
