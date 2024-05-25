4d119984f8ab261e30c066b2df44e2683580ff78
roslove44 <rostandmigan68@gmail.com>
2024-05-25 13:16:56 +0200

    Works Display

--- src/Controller/WorkController.php
+++ src/Controller/WorkController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use App\Entity\Project;
+use App\Repository\FilterRepository;
+use App\Repository\ProjectRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -10,15 +12,14 @@ use Symfony\Component\Routing\Attribute\Route;
 class WorkController extends AbstractController
 {
     #[Route('/work', name: 'app_work')]
-    public function index(): Response
+    public function index(ProjectRepository $projectRepository, FilterRepository $filterRepository): Response
     {
-        return $this->render('work/index.html.twig', []);
-    }
-
-    #[Route('/work/details', name: 'app_work_details')]
-    public function details(): Response
-    {
-        return $this->render('work/details.html.twig', []);
+        $projects = $projectRepository->findAll();
+        $filters = $filterRepository->findAll();
+        return $this->render('work/index.html.twig', [
+            'works' => $projects,
+            'filters' => $filters,
+        ]);
     }
 
     #[Route('/work/{slug}', name: 'app_work_detail')]
